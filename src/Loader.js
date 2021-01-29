1d9ad95c8be0e3fed6429f3a21f007822b366d07
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 23:20:25 +0500

    start

---
+++ src/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+    <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
+        <div class="lds-circle"><div></div></div>
+    </div>
+)
