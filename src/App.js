687f483a0ee22afb1263f90e2979f8b44319d436
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-30 11:47:12 +0500

    fetch

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
-import React, { useEffect } from 'react'
-import TodoList from './Todo/TodoList'
-import Context from './context'
-//import AddTodo from './Todo/AddTodo'
-import Loader from './Loader'
-import Modal from './Modal/Modal'
-const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
+import React, { useEffect, useState } from 'react'
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/"
+const url = 'http://api.football-data.org/v2/competitions'
+const getJson = () => {
+  const response = fetch(proxyurl+url)
+                    .then(response => response.text())
+                    //.then(json => JSON.stringify(json))
+  //const res = JSON.stringify(response, null, ' ')
+  return response
+  
+}
 
 function App() {
-  const [todos, setTodos] = React.useState([])
-  const [loading, setLoading] = React.useState(true)
+  const [val, setVal] = useState('')
   
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(response => response.json())
-      .then(todos => {
-        setTimeout(() => {
-          setTodos(todos)
-          setLoading(false)
-        }, 2020)
-      })
-  }, [])
-
-  function toggleTodo(id) {
-    setTodos(todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    }))
-  }
-
-  function removeTodo(id) {
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
-
-  function addTodo(title) {
-    setTodos(todos.concat([{
-      title,
-      id: Date.now(),
-      completed: false
-    }]))
+  const handleClick = () => {
+    setVal(getJson())
   }
   return (
-    <Context.Provider value={{ removeTodo }}>
-      <div className='wrapper'>
-        <h1>React tutorial</h1>
-        <Modal/>
-        <React.Suspense fallback={<p>Loading...</p>}>
-          <AddTodo onCreate={addTodo}/>
-        </React.Suspense>
-        {loading && <Loader />}
-
-        { todos.length !== 0 ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : 
-        loading ? null : (<p>No todos!</p>)}
-      </div>
-    </Context.Provider>
+    <div>
+      <input></input>
+      <button onClick={handleClick}>Найти</button>
+      <pre>{val}</pre>
+    </div>
   );
 }
 
