705d1d4614b2db8d64afeb48694a218b5bd6cd67
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-30 12:41:11 +0500

    get json

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/"
 const url = 'http://api.football-data.org/v2/competitions'
-const getJson = () => {
-  const response = fetch(proxyurl+url)
-                    .then(response => response.text())
-                    //.then(json => JSON.stringify(json))
-  //const res = JSON.stringify(response, null, ' ')
-  return response
-  
+const getJson = async () => {
+  /* const response = fetch(proxyurl+url)
+                    .then(response => response.json())
+  const res = JSON.stringify(response, null, ' ')
+  return res */
+  let resp = await fetch(proxyurl+url)
+  let text = await resp.json()
+  console.log(JSON.stringify(text))
+  return text
 }
 
 function App() {
@@ -21,7 +23,7 @@ function App() {
     <div>
       <input></input>
       <button onClick={handleClick}>Найти</button>
-      <pre>{val}</pre>
+      <div>{JSON.stringify(val, null, ' ')}</div>
     </div>
   );
 }
