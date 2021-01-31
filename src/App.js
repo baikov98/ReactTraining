842990ee4ee9173652c9cba4b3d78af39491283a
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-31 13:07:27 +0500

    table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from 'react'
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/"
-const url = 'http://api.football-data.org/v2/competitions/'
+const url = 'http://api.football-data.org/v2/competitions'
 
-const getJson = () => {
+/* function CompetitionItem(props) {
 
-  fetch(url, {headers: { 'X-Auth-Token': 'e161b5cf73d24b83bad26a7af72478e1' }})
-        .then(response => response.json())
-        .then(json => console.log(json))
-
-
-}
-
-function CompetitionList(props) {
   return (
-    <div>{props.response.toString()}</div>
+    
   )
-  
+} */
+
+function CompetitionsList(props) {
+  console.log(props.response.competitions[1])
+  return (
+    <table>
+      <thead>
+      <tr>
+        <td>Area</td>
+        <td>Country Code</td>
+        <td>League</td>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  )
 }
+
+
 
 function App() {
   const [val, setVal] = useState(null)
@@ -25,7 +36,7 @@ function App() {
   const handleClick = () => {
     fetch(url, {headers: { 'X-Auth-Token': 'e161b5cf73d24b83bad26a7af72478e1' }})
         .then(response => response.json())
-        .then(json => setVal(json.count))
+        .then(json => setVal(json))
   }
     
   
@@ -33,7 +44,7 @@ function App() {
     <div>
       <input></input>
       <button onClick={handleClick}>Найти</button>
-      {val ? <div><CompetitionList response={val} /></div> : <div> Loading...</div>}
+      {val ? <div><CompetitionsList response={val} /></div> : <div> Loading...</div>}
       
     </div>
   );
