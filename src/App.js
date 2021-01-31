e55d686b7a13decff3f470e1a1cd257bb3ef6a51
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-31 14:41:11 +0500

    avaliable ids

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react'
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/"
 const url = 'http://api.football-data.org/v2/competitions'
+const teams = 'http://api.football-data.org/v2/competitions/2003/standings'
+
+function getTeams() {
+  fetch(teams, {headers: { 'X-Auth-Token': 'e161b5cf73d24b83bad26a7af72478e1' }})
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
 
 function CompetitionItem(props) {
 
@@ -15,15 +22,18 @@ function CompetitionItem(props) {
 } 
 
 function CompetitionsList(props) {
-  console.log(props.response.competitions[1])
+  //console.log(props.response.competitions[1])
+  getTeams()
   const leagues = props.response.competitions
+  const availableIDs = [2000,2001,2002,2003,2013,2014,2015,2016,2017,2018,2019,2021]
   return (
     <table>
       <thead>
       <tr>
-        <td>Area</td>
-        <td>Country Code</td>
-        <td>League</td>
+        <th>Region</th>
+        <th>Country Code</th>
+        <th>League</th>
+        <th>League1</th>
         </tr>
       </thead>
       <tbody>
