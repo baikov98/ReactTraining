d7cd76c8f0b2912a59d834497692fadaa3fe8b46
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-02-07 21:41:19 +0500

    year filter for main page is ready

--- src/pages/CompetitionsList/CompetitionsList.js
+++ src/pages/CompetitionsList/CompetitionsList.js
@@ -7,73 +7,42 @@ import {
 
 import SearchInput from '../../components/SearchInput/SearchInput'
 import CompetitionsTable from './CompetitionsTable'
+import YearSelect from '../../components/YearSelect/YearSelect'
 
-const matches = 'http://api.football-data.org/v2/competitions/2021/matches'
+
 const url = 'http://api.football-data.org/v2/competitions'
 const availableIDs = [2000, 2001, 2002, 2003, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]
 
-/* function getIt() {
-  fetch(matches, {headers: { 'X-Auth-Token': 'e161b5cf73d24b83bad26a7af72478e1' }})
-        .then(response => response.json())
-        .then(json => console.log(json))
-} */
-
-function CompetitionItem(props) {
-    return (
-      <tr>
-        <td>{ props.iconUrl ? <img src={props.iconUrl} className='country__icon'/> : null}</td>
-        <td>{props.area}</td>
-        <td>{props.ccode}</td>
-        <td>{props.league}</td>
-        <td>{props.teamLink}</td>
-        <td>{props.calLink}</td>
-        <td>{props.startDate}</td>
-        <td>{props.endDate}</td>
-      </tr>
-    )
-} 
   
 export default function CompetitionsList(props) {
-
-    const [data, setData] = useState(null)
+    const loc = new URLSearchParams(window.location.search)
     let location = useLocation()
+    const [data, setData] = useState(null)
+    const [year, setYear] = useState(loc.get('year') || '2020')
+    const yearSwitcher = (year) => {
+      setYear(year);
+      console.log('SWITCH', year)
+  }
     useEffect(() => {console.log(location)}, [location.search])
     useEffect(() => {
-      console.log('competitions')
-      //getIt()
       fetch(url, {headers: { 'X-Auth-Token': 'e161b5cf73d24b83bad26a7af72478e1' }})
           .then(response => response.json())
           .then(json => setData(json.competitions))
     }, [])
     
-
     if (!data) { return <div>Loading...</div>}
 
     const leagueArr = data.filter((val) => {
                       return availableIDs.includes(val.id)
                       })
-    
     const query = new URLSearchParams(window.location.search).get('query') || '';
-    const ItemsCompetition = leagueArr
-                    .filter((val, i) => {
-                    if (query === '') return val;
-                    if (val.name.toLowerCase().includes(query.toLowerCase())) return val })
-                    .map((val, i) => ( 
-                        <CompetitionItem iconUrl={val.area.ensignUrl} 
-                                         area={val.area.name} 
-                                         ccode={val.area.countryCode} 
-                                         league={val.name} 
-                                         teamLink={<Link to={`/${val.id}/teams`}>Teams</Link>}
-                                         calLink={<Link to={`/${val.id}/calendar`}>Calendar</Link>}
-                                         startDate={val.currentSeason.startDate}
-                                         endDate={val.currentSeason.endDate}
-                                         key={i}/>
-                                        ))
+    
     console.log(leagueArr)
     return (
       <>
       <SearchInput />
-      <CompetitionsTable leagueArr={leagueArr} query={query} />
+      <YearSelect yearSwitcher={yearSwitcher} yearArray={[2020, 2019, 2018, 2017]} />
+      <CompetitionsTable leagueArr={leagueArr} query={query} year={year} />
       </>
     )
   }
--- src/pages/CompetitionsList/CompetitionsTable.js
+++ src/pages/CompetitionsList/CompetitionsTable.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom"; 
 
+const availableIDs = [2000, 2001, 2002, 2003, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]
+
 function TableItem({ i }) {
     return (<tr>
                 <td>{ i.area.ensignUrl ? <img src={i.area.ensignUrl} className='country__icon'/> : null}</td>
                 <td>{i.area.name}</td>
                 <td>{i.area.countryCode}</td>
                 <td>{i.name}</td>
-                <td>{<Link to={`/${i.id}/teams`}>Teams</Link>}</td>
-                <td>{<Link to={`/${i.id}/calendar`}>Calendar</Link>}</td>
-                <td>{i.currentSeason.startDate}</td>
-                <td>{i.currentSeason.endDate}</td>
+                <td>{availableIDs.includes(i.id) ? 
+                                (<Link to={`/${i.id}/teams`}>Teams</Link>) : 'Teams'}</td>
+                <td>{availableIDs.includes(i.id) ? 
+                                (<Link to={`/${i.id}/calendar`}>Teams</Link>) : 'Teams'}</td>
+                <td>{i.currentSeason?.startDate || ''}</td>
+                <td>{i.currentSeason?.endDate || ''}</td>
             </tr>)
 }
 
@@ -36,12 +40,16 @@ function LeagueTable({ children }) {
     )
 }
 
-export default function CompetitionsTable({ leagueArr, query }) {
+export default function CompetitionsTable({ leagueArr, query, year }) {
     const FilteredByQuery = leagueArr
                     .filter((val, i) => {
                     if (query === '') return val;
                     if (val.name.toLowerCase().includes(query.toLowerCase())) return val })
-    const ItemsCompetition = FilteredByQuery.map((i, index) => (
+    const FilteredByYear = FilteredByQuery
+                    .filter((val) => {
+                        return new Date(val.currentSeason.startDate).getFullYear() == year
+                    })
+    const ItemsCompetition = FilteredByYear.map((i, index) => (
         <TableItem key={index} i={i} />
     ))
     return (
