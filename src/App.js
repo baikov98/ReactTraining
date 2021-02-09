883d3768e183c69f949dd0959e543b5edeee919d
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-02-09 13:05:46 +0500

    context created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
+  useHistory
 } from "react-router-dom";
+import Context from './context' 
 
 import CompetitionsList from './pages/CompetitionsList/CompetitionsList'
 import TeamList from './pages/TeamList/TeamList'
@@ -14,8 +16,30 @@ import PageNotFound from './pages/PageNotFound/PageNotFound'
 import Team from './pages/Team/Team'
 
 function App() {
+  const history = useHistory()
+  const location = new URLSearchParams(window.location.search)
+  console.log(location)
+  const [searchQuery, setSearchQuery] = useState(location.get('query'))
+  useEffect(() => {
+    console.log('refresh')
+  }, [window.location.search])
+  const setQuery = (history, name, data) => {
+    const loc = new URLSearchParams(window.location.search)
+    if (!loc.has(name)) loc.append(name, data);
+    else loc.set(history, name, data);
+    history.push({search: loc.toString()})
+  }
+  const deleteQuery = (history, name) => {
+    const loc = new URLSearchParams(window.location.search)
+    loc.delete(name)
+    history.push({search: loc.toString()})
+  }
+
 
   return (
+    <Context.Provider value={
+      {setQuery, deleteQuery}
+    }>
     <Router>
     <h1>Top soccer tournaments statistics</h1>
     <h3><Link to='/'>Главная</Link></h3>
@@ -32,6 +56,7 @@ function App() {
 
       </Switch>
     </Router>
+    </Context.Provider>
   );
 }
 
--- src/pages/CompetitionsList/CompetitionsList.js
+++ src/pages/CompetitionsList/CompetitionsList.js
@@ -1,10 +1,11 @@
-import { useEffect, useState,  } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import {
   Link,
   useParams,
-  useLocation
+  useLocation,
+  useHistory
 } from "react-router-dom"; 
-
+import Context from '../../context'
 import SearchInput from '../../components/SearchInput/SearchInput'
 import CompetitionsTable from './CompetitionsTable'
 import YearSelect from '../../components/YearSelect/YearSelect'
@@ -15,6 +16,8 @@ const availableIDs = [2000, 2001, 2002, 2003, 2013, 2014, 2015, 2016, 2017, 2018
 
   
 export default function CompetitionsList(props) {
+    const { setQuery, deleteQuery } = useContext(Context)
+    const history = useHistory()
     const loc = new URLSearchParams(window.location.search)
     const query = loc.get('query') || '';
     const yearArray = [2020, 2019, 2018, 2017]
@@ -24,8 +27,9 @@ export default function CompetitionsList(props) {
       setYear(year);
       console.log('SWITCH', year)
     }
-    useEffect(() => {console.log(loc)}, [window.location.search])
+    useEffect(() => {console.log('useEF for window.loc')}, [window.location.search])
     useEffect(() => {
+      console.log('useEF for FETCH')
       fetch(url, {headers: { 'X-Auth-Token': 'e161b5cf73d24b83bad26a7af72478e1' }})
           .then(response => response.json())
           .then(json => setData(json))
@@ -40,6 +44,8 @@ export default function CompetitionsList(props) {
     console.log(data)
     return (
       <>
+      <button onClick={() => setQuery(history, 'rer', 'lodp')}>asd</button>
+      <button onClick={() => deleteQuery(history, 'rer')}>asd</button>
       <SearchInput />
       <YearSelect yearSwitcher={yearSwitcher} yearArray={yearArray} />
       <CompetitionsTable leagueArr={leagueArr} query={query} year={year} />
