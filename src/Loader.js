8d972398b0f55df4449e9caf9340054f756a80c3
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-02-14 21:10:25 +0500

    gh-pages

--- src/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default () => (
-    <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
-        <div class="lds-circle"><div></div></div>
-    </div>
-)
--- src/components/Loader/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Loader = () => {
-    return (<div className="lds-dual-ring"></div>)
-}
-
-export default Loader
